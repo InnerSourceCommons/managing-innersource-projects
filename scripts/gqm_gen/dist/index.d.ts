56cc095f4d4f895c0d0f59719ecc6cddae95dee4
Jeff Bailey <776901+jeffabailey@users.noreply.github.com>
2025-05-10 13:50:11 -0700

    chore: remove dist dir and ignore

--- scripts/gqm_gen/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as Commonmark from "commonmark";
-import { Graph, Link, LinkType } from "./types.js";
-import { FileLink } from "./types.js";
-export declare function getLinkUrl(linkType: LinkType, file: string): string;
-export declare function getGQMFileLinks(mdFilePath: string): Graph;
-export declare function getFileLinks(linkType: LinkType, filePath: string): FileLink[];
-export declare function getLinks(parsed: Commonmark.Node): Link[];
-export declare function generateMermaidDiagram(graph: Graph): string;
-export declare function updateGQMDiagram(): void;
