9dcca5b37c96a552064490358d5324c2e24a025f
jeffabailey <jeffabailey@users.noreply.github.com>
2026-03-19 02:39:21 +0000

    Deploy preview for PR 111 🛫

--- pr-preview/pr-111/html/searcher-c2a407aa.js
+++ pr-preview/pr-111/html/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-867a60fa.js',
+                path_to_root + 'searchindex-e54354ab.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
